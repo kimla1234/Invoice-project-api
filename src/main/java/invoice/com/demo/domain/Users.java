eb402d97cc35c268672cb7dfcc81b6c42639529a
kimla1234 <chhoeurn.kimla.1221@rupp.edu.kh>
2026-02-03 14:23:49 +0700

    first commit

--- src/main/java/invoice/com/demo/domain/Users.java
+++ src/main/java/invoice/com/demo/domain/Users.java
@@ -1,4 +1,44 @@
 package invoice.com.demo.domain;
 
-public class Users {
+import invoice.com.demo.audit.Auditable;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="users")
+public class Users extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String uuid;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(length = 30)
+    private String email;
+
+    private String password;
+    private String image_profile;
+
+    @Column(length = 30)
+    private String phone_number;
+
+    @Column(name = "is_delete", nullable = false)
+    private Boolean isDelete = false;
+
+    private String verificationCode;
+
+    private Boolean status = true;
+    private Boolean isVerified = false;
+
 }
